2370209bd4a3625ecefc90f7c76e61b2a4aee3d6
kafu24 <90677839+kafu24@users.noreply.github.com>
2023-10-09 04:38:56 -0700

    Axios code and CORS

--- react-frontend/src/MyApp.js
+++ react-frontend/src/MyApp.js
@@ -1,5 +1,6 @@
 // src/MyApp.js
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import Table from './Table';
 import Form from './Form';
 
@@ -7,15 +8,33 @@ function MyApp() {
   const [characters, setCharacters] = useState([]);
   
   function removeOneCharacter (index) {
-      const updated = characters.filter((character, i) => {
-          return i !== index
-      });
+    const updated = characters.filter((character, i) => {
+      return i !== index
+    });
     setCharacters(updated);
   }
 
   function updateList(person) {
     setCharacters([...characters, person]);
   }
+
+  async function fetchAll() {
+    try {
+      const response = await axios.get('http://localhost:8000/users');
+      return response.data.users_list;
+    } catch (error) {
+      // We're not handling errors. Just logging into the console.
+      console.log(error);
+      return false;
+    }
+  }
+
+  useEffect(() => {
+    fetchAll().then( result => {
+      if (result)
+        setCharacters(result);
+    });
+  }, [] );
     
   return (
     <div className="container">
