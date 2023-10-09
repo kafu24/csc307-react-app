cf908220405c93f2bbfa7debb7142aae923e3fe5
kafu24 <90677839+kafu24@users.noreply.github.com>
2023-10-09 04:43:48 -0700

    Submit button and POST calls to add users

--- react-frontend/src/MyApp.js
+++ react-frontend/src/MyApp.js
@@ -15,7 +15,10 @@ function MyApp() {
   }
 
   function updateList(person) {
-    setCharacters([...characters, person]);
+    makePostCall(person).then( result => {
+      if (result && result.status === 200)
+        setCharacters([...characters, person] );
+    });
   }
 
   async function fetchAll() {
@@ -35,7 +38,17 @@ function MyApp() {
         setCharacters(result);
     });
   }, [] );
-    
+
+  async function makePostCall(person) {
+    try {
+      const response = await axios.post('http://localhost:8000/users', person);
+      return response;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+
   return (
     <div className="container">
       <Table characterData={characters} 
